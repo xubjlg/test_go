c1d0c2cda2505a7f877de9042836fa0d9e9cd45c
xubj <xubj@ickey.cn>
2020-03-09 00:34:47 +0800

    commit

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/go-ffmt/ffmt"
+	"time"
 )
 
 var (
@@ -19,7 +20,12 @@ func main() {
 
 	//测试 全局变量
 	main3()
-	ffmt.Print(b)
+	_, _ = ffmt.Print(b)
+
+	b = 5
+	_, _ = ffmt.Print(b)
+
+	baseTime()
 }
 
 
@@ -30,30 +36,83 @@ func main1() {
 
 	fmt.Printf("%s\n",a)
 
-	ffmt.Print(a)
+	_, _ = ffmt.Print(a)
 }
 
 func main2() {
-	ffmt.Print(a)
+	_, _ = ffmt.Print(a)
 	a := 1
 
-	ffmt.Print(a)
+	_, _ = ffmt.Print(a)
 	{
 		a := 2
-		ffmt.Print(a)
+		_, _ = ffmt.Print(a)
 	}
 
-	ffmt.Print(a)
+	_, _ = ffmt.Print(a)
 }
 
 func main3() {
-	ffmt.Print(b)
+	_, _ = ffmt.Print(b)
 
 	b = 3
 	{
 		b := 4
-		ffmt.Print(b)
+		_, _ = ffmt.Print(b)
 	}
 
-	ffmt.Print(b)
+	_, _ = ffmt.Print(b)
+}
+
+func baseTime() {
+	t := time.Now()
+
+	_, _ = ffmt.Print(t)
+//	fmt.Printf(t)
+
+	fmt.Println(t)
+	fmt.Printf("%02d.%02d.%4d\n", t.Day(),t.Month(),t.Year())
+
+	t = time.Now().UTC()
+	fmt.Println(t)
+
+	tt := 5 * time.Second
+
+	_, _ = ffmt.Print(tt)
+
+	week := 60 * 60 * 24 * 7 * 1e9
+
+	newWeek := t.Add(time.Duration(week))
+	fmt.Println(newWeek)
+
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+
+	fmt.Println(time.Now())
+
+	lo, _ := time.LoadLocation("")
+	fmt.Println(lo)
+
+
+	ticker  := time.Tick(1 * time.Second)
+
+	for _ = range ticker {
+		fmt.Println(1 )
+	}
+
+
+	newTicker := time.NewTicker(5 * time.Second)
+
+	newTicker.Stop()  // todo  如何开启 如何停用
+
+
+newTime(1 * time.Second)
+
+}
+
+
+func newTime(d time.Duration) time.Timer {
+	t := time.Timer{}
+	fmt.Println("time")
+
+	return t
 }
